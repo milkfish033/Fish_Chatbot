16806f9cb93371851359eb38996c5d22406a4cde
Kerwin <141232455+milkfish033@users.noreply.github.com>
2024-07-04 14:44:31 +0800

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import llm
+
+
+st.title("MilkFish ChatBot🐟")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+#Generate and select response
+
+def response_generator(text):
+    response = llm.QA(text)
+    return response
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response_generator(prompt))
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response_generator(prompt)})
+
